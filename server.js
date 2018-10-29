e1ce690555fe497719a81f31ee47bc0f6921d240
DAmini <amini.daniel@gmail.com>
2018-10-28 20:20:13 -0700

    Add Initial File & Folder Structures

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const bodyparser = require('body-parser')
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyparser.urlencoded({extended: true}))
+app.use(bodyparser.json())
+
+app.get('/', (req, res) => res.render('index'))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
---
+++ views/index.jsx
@@ -0,0 +1,20 @@
+const React = require('react')
+
+const Page = () =>
+    <html lang="en">
+        <head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
+            <link rel="stylesheet" href="./style.css" />
+            <title>Document</title>
+        </head>
+        <body>
+            <h1>Hello Multiverse!</h1>
+
+
+            
+        </body>
+    </html>    
+
+module.exports = Page
